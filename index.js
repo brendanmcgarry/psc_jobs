bd7e2b631c4fd267387fb6bc3ac45a1e587d96a6
brendanmcgarry <brendanmcgarry13@gmail.com>
2019-03-23 19:42:59 -0400

    Added mongoose.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017')
+
 
 app.get('/', (req, res) => {
 	// GET request for homepage
+	res.send('Hi!');
 })
 
 app.post('/jobs/create', (req, res) => {
@@ -20,9 +25,9 @@ app.put('/jobs/update', (req, res) => {
 	// authorization required of course
 })
 
-var server = app.listen(8000, () => {
-	var host = server.address().address;
-	var port = server.address().port;
+const server = app.listen(8000, () => {
+	const host = server.address().address;
+	const port = server.address().port;
 
 	console.log(`PSC Jobs site listening at http://${host}:${port}`)
 })
