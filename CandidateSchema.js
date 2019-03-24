0cf2e56dbc218413ccc51c752a0a70963ae372ce
arejayelle <35614138+arejayelle@users.noreply.github.com>
2019-03-23 20:08:55 -0400

    fixed formatting for mongoose

--- CandidateSchema.js
+++ CandidateSchema.js
@@ -3,17 +3,15 @@ const ObjectId = Schema.ObjectId;
 
 const CandidateSchema = new Schema(
     {
-    "id":Number,  //Unique ID
-    "first_name": String, //First name of candidate
-    "last_name": String, //Last name of candidate
-    "universe_permits":Array, //Array listing all Universe’s this candidate can apply
-    "willing_to_do_overtime": Bool, // Willing to do overtime
-    "willing_to_relocate_to_other_universe": Bool, //Willing to relocate to another univers
-    "home_address": String , // Address in String unformatted
-    "education_level": Number, // ID education level
-    "skills": Array // All skills tied to this candidate
-
-
+    id:Number,  //Unique ID
+    first_name: String, //First name of candidate
+    last_name: String, //Last name of candidate
+    universe_permits:Array, //Array listing all Universe’s this candidate can apply
+    willing_to_do_overtime: Bool, // Willing to do overtime
+    willing_to_relocate_to_other_universe: Bool, //Willing to relocate to another univers
+    home_address: String , // Address in String unformatted
+    education_level: Number, // ID education level
+    skills: Array // All skills tied to this candidate
 });
 
 // CandidateSchema.methods.update = function(attribute, newValue) {
